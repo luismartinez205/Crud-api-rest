f93b06eec5915c2c048d12d63622792068df77a5
luismartinez205 <luisfrancisco198@gmail.com>
2026-06-30 21:17:22 -0500

    ajuste en firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,15 @@
-import { initializeApp, cert, getApps } from "firebase-admin/app";
+import { cert, initializeApp, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceAccountJson = process.env.FIREBASE_SERVICE_ACCOUNT;
-if (!serviceAccountJson) {
-  throw new Error('FIREBASE_SERVICE_ACCOUNT environment variable is required');
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
+
+// inicializa solo si no existe app
+if (!getApps().length) {
+  initializeApp({
+    credential: cert(serviceAccount),
+  });
 }
 
-let serviceAccount;
-try {
-  serviceAccount = JSON.parse(serviceAccountJson);
-} catch (err) {
-  throw new Error('FIREBASE_SERVICE_ACCOUNT is not valid JSON');
-}
-
-const app = getApps().length
-  ? getApps()[0]
-  : initializeApp({
-      credential: cert(serviceAccount),
-    });
-
-const db = getFirestore(app);
-
-export { db };
+export const db = getFirestore();
